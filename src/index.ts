8d5c9635deea6a464660c6cb4ca66794fe34852b
Kishan Jadav <kishan_jadav@hotmail.com>
2026-01-11 11:57:09 +0000

    feat: export PdfViewProps type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 // Reexport the native module
 export { default } from './pdf-module';
-export { PdfView } from './pdf-view';
+export { PdfView, PdfViewProps } from './pdf-view';
 export * from './types';
